24d59d3adcd533e1c75395c678d7391a213eb997
EtornamagbemabieseE <etornama@unc.edugit config --global user.name Etornamagbemabiese   etornam@etornams-mbp comp110-23f-workspace % git config --global user.name Etornamagbemabiese>
2026-03-04 17:33:07 -0500

    configure

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === "production";
-const repoName = "/blackinnovatorsconference.github.io";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
-  basePath: isProduction ? repoName : "",
-  assetPrefix: isProduction ? `${repoName}/` : "",
 };
 
 export default nextConfig;
