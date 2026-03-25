f4146b55a3c9adbb78e5f440555c87da9284ffb7
Etornam Agbemabiese <142925756+Etornamagbemabiese@users.noreply.github.com>
2026-03-25 18:36:37 -0400

    Update team-data.ts

--- src/lib/team-data.ts
+++ src/lib/team-data.ts
@@ -62,7 +62,7 @@ export const TEAM_MEMBERS: TeamMember[] = [
     ],
   },
   {
-    name: "Candice",
+    name: "Grace Odondi",
     image: "/headshots/grace.png",
     title: "Programming & Experience Lead",
     bio: [
@@ -72,7 +72,7 @@ export const TEAM_MEMBERS: TeamMember[] = [
     ],
   },
   {
-    name: "Grace",
+    name: "Vanessa Walcott",
     image: "/headshots/vanessa.png",
     title: "Partnerships & Outreach",
     bio: [
@@ -82,7 +82,7 @@ export const TEAM_MEMBERS: TeamMember[] = [
     ],
   },
   {
-    name: "Vanessa",
+    name: "Candice Bamba",
     image: "/headshots/candice.png",
     title: "Operations & Logistics",
     bio: [
